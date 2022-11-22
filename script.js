a8820a40e2fe67d88982f7fe971b0b67a02c27c9
SkyeH-M <justskye98@gmail.com>
2022-11-22 10:46:26 +0000

    Feat: added aria-labels to social network links

---
+++ script.js
@@ -0,0 +1,19 @@
+let mybutton = document.getElementById("btn-to-top");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+mybutton.addEventListener("click", backToTop);
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
